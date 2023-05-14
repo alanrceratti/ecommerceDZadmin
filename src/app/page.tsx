a7fabbba572fa541a10d4dc914ab12bda269ec93
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-05-14 06:16:04 +0100

    test vercel not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import { User } from "@/components/user.component";
 import Hero from "./hero/page";
 // import { getServerSession } from "next-auth";
 // import { authOptions } from "../../lib/auth";
 
 export default function App() {
-	// const session = await getServerSession(authOptions);
+	// const session = getServerSession(authOptions);
 	// console.log(session);
 	return (
 		<main>
 			<Hero />;
-			<User />;
 		</main>
 	);
 }
