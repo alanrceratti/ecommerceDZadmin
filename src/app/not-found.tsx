5bad7813c3ed4683c8b927511f40ee9159e1c56d
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-05-28 21:54:44 +0100

    404 error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,34 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NotFound() {
 	return (
-		<div>
-			<h2>Not Found</h2>
-			<p>Could not find requested resource</p>
-			<p>
-				View <Link href="/blog">all posts</Link>
-			</p>
-		</div>
+		<>
+			<div className="bg-[#FDF6E4] h-screen flex justify-center w-full ml-auto mr-auto">
+				<div className="text-center text-2xl pt-8">
+					<h2 className="text-3xl">404 page not found</h2>
+					<div className="w-[512px] h-[340px] relative m-auto ">
+						<Image
+							src="/assets/notfound.webp"
+							alt="not-found"
+							fill
+						/>
+					</div>
+
+					<p className="w-2/3 m-auto">
+						Ops... It looks like this drone has ventured into
+						uncharted territory and It got lost while exploring a
+						forbidden place...
+					</p>
+					<br></br>
+					<p className=" m-auto">
+						View Help us bring it back safely by clicking&nbsp;
+						<Link href="/" className="text-blue-500">
+							here
+						</Link>
+					</p>
+				</div>
+			</div>
+		</>
 	);
 }
