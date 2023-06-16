5f50afacff8173521df45b73e14ec4728885a1c1
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-06-16 10:26:16 +0100

    changing loading path

--- src/app/(pages)/(front)/products/[[...categorySelected]]/page.tsx
+++ src/app/(pages)/(front)/products/[[...categorySelected]]/page.tsx
@@ -1,10 +1,4 @@
-// "use client";
-import CategoriesFilter from "@/components/front/products/categoriesFilter";
-import Loading from "@/components/front/products/loading";
 import MainCard from "@/components/front/products/maincard";
-import ProductsFilter from "@/components/front/products/productsFilter";
-// import { useParams, usePathname } from "next/navigation";
-import { Suspense } from "react";
 
 export default function Products() {
 	return (
