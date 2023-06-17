d7df5c3ea0d75215a73e6a0cda7f17be75b763cc
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-06-17 21:24:16 +0100

    category rendering issue mobile5

--- src/components/front/products/categoriesFilter.tsx
+++ src/components/front/products/categoriesFilter.tsx
@@ -129,7 +129,7 @@ export default function CategoriesFilter() {
 
 	return (
 		<main className="w-fit ">
-			{allCategories.length > 0 && mobile ? (
+			{result.length > 0 && mobile ? (
 				<div className="m-2 font-poppins ">
 					{mobile && (
 						<>
@@ -164,7 +164,7 @@ export default function CategoriesFilter() {
 									>
 										Show All
 									</button>
-									{allCategories && result.length > 0 && (
+									{result && result.length > 0 && (
 										<div className="bg-black text-white  h-fit rounded-md font-poppins py-1">
 											{result.map((category, index) => (
 												<Link
