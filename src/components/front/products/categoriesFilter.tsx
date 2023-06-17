16863b29a32fef59dadb1c5b22b4d0218f35ae71
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-06-17 20:44:55 +0100

    category rendering issue mobile3

--- src/components/front/products/categoriesFilter.tsx
+++ src/components/front/products/categoriesFilter.tsx
@@ -122,7 +122,7 @@ export default function CategoriesFilter() {
 
 	return (
 		<main className="w-fit ">
-			{resultsUpdate && categoriesCount.length > 0 && mobile ? (
+			{allCategories.length > 0 && mobile ? (
 				<div className="m-2 font-poppins ">
 					{mobile && (
 						<>
@@ -157,33 +157,37 @@ export default function CategoriesFilter() {
 									>
 										Show All
 									</button>
-									{result && result.length > 0 && (
-										<div className="bg-black text-white  h-fit rounded-md font-poppins py-1">
-											{result.map((category, index) => (
-												<Link
-													onClick={() =>
-														setIsOpen(false)
-													}
-													href={`/products/filter?category=${category?.key}`}
-													key={category?.key}
-													className={`${
-														categoryPath ===
-														category?.key
-															? active
-															: notActive
-													} flex w-full p-2 hover:text-orange ${
-														index !==
-														categories.length - 1
-															? "border-b-2 border-gray-700"
-															: ""
-													}`}
-												>
-													{category?.value}
-													{category?.count}
-												</Link>
-											))}
-										</div>
-									)}
+									{allCategories &&
+										allCategories.length > 0 && (
+											<div className="bg-black text-white  h-fit rounded-md font-poppins py-1">
+												{result.map(
+													(category, index) => (
+														<Link
+															onClick={() =>
+																setIsOpen(false)
+															}
+															href={`/products/filter?category=${category?.key}`}
+															key={category?.key}
+															className={`${
+																categoryPath ===
+																category?.key
+																	? active
+																	: notActive
+															} flex w-full p-2 hover:text-orange ${
+																index !==
+																categories.length -
+																	1
+																	? "border-b-2 border-gray-700"
+																	: ""
+															}`}
+														>
+															{category?.value}
+															{category?.count}
+														</Link>
+													)
+												)}
+											</div>
+										)}
 								</div>
 							) : null}
 						</>
