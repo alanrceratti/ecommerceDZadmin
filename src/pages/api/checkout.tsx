e07371121b59f2d507809d1d6004d35efc522a54
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-07-04 21:39:43 +0100

    checkout problem testing9

--- src/pages/api/checkout.tsx
+++ src/pages/api/checkout.tsx
@@ -31,48 +31,48 @@ export default async function handle(
 			return;
 		}
 		try {
-			await mongooseConnect();
-			const productsIds = products.split(",");
-			const uniqueIds = [...new Set(productsIds)];
-			const productsInfo = await Product.find({ _id: uniqueIds });
+			// 	await mongooseConnect();
+			// 	const productsIds = products.split(",");
+			// 	const uniqueIds = [...new Set(productsIds)];
+			// 	const productsInfo = await Product.find({ _id: uniqueIds });
 
-			let line_items = [];
-			for (const productId of uniqueIds) {
-				const productInfo = productsInfo.find(
-					(p) => p._id.toString() === productId
-				);
-				const quantity =
-					productsIds.filter((id: string) => id === productId)
-						?.length || 0;
-				if (quantity > 0 && productInfo) {
-					line_items.push({
-						quantity,
-						price_data: {
-							currency: "GBP",
-							product_data: { name: productInfo.name },
-							unit_amount: quantity * productInfo.price,
-						},
-					});
-				}
-			}
-			const orderDoc = await Order.create({
-				line_items,
-				name: session.data.user.name,
-				email: session.data.user.email,
-				paid: false,
-			});
+			// 	let line_items = [];
+			// 	for (const productId of uniqueIds) {
+			// 		const productInfo = productsInfo.find(
+			// 			(p) => p._id.toString() === productId
+			// 		);
+			// 		const quantity =
+			// 			productsIds.filter((id: string) => id === productId)
+			// 				?.length || 0;
+			// 		if (quantity > 0 && productInfo) {
+			// 			line_items.push({
+			// 				quantity,
+			// 				price_data: {
+			// 					currency: "GBP",
+			// 					product_data: { name: productInfo.name },
+			// 					unit_amount: quantity * productInfo.price,
+			// 				},
+			// 			});
+			// 		}
+			// 	}
+			// 	const orderDoc = await Order.create({
+			// 		line_items,
+			// 		name: session.data.user.name,
+			// 		email: session.data.user.email,
+			// 		paid: false,
+			// 	});
 
-			const sessionStripe = await stripe.checkout.sessions.create({
-				line_items,
-				mode: "payment",
-				customer_email: session.data.user.email,
-				payment_method_types: ["card"],
-				success_url: `${req.headers.origin}/cart?success=1`,
-				cancel_url: `${req.headers.origin}/cart?cancel=1`,
-				metadata: {
-					orderId: orderDoc._id.toString(),
-				},
-			});
+			// 	const sessionStripe = await stripe.checkout.sessions.create({
+			// 		line_items,
+			// 		mode: "payment",
+			// 		customer_email: session.data.user.email,
+			// 		payment_method_types: ["card"],
+			// 		success_url: `${req.headers.origin}/cart?success=1`,
+			// 		cancel_url: `${req.headers.origin}/cart?cancel=1`,
+			// 		metadata: {
+			// 			orderId: orderDoc._id.toString(),
+			// 		},
+			// 	});
 			// res.setHeader(
 			// 	"Access-Control-Allow-Methods",
 			// 	"GET,OPTIONS,PATCH,DELETE,POST,PUT"
@@ -81,8 +81,9 @@ export default async function handle(
 			// 	"Access-Control-Allow-Headers",
 			// 	"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
 			// );
-			res.status(200).json(sessionStripe);
-			console.log("URLllll", sessionStripe.url);
+			// res.status(200).json(sessionStripe);
+			// console.log("URLllll", sessionStripe.url);
+			console.log("FUNFANDOOO");
 		} catch (error: any) {
 			console.log(error);
 			res.status(500).json({
