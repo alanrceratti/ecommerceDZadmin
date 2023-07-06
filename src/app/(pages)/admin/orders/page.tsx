a97d6a89ec1d3c8ba56dc8586510693920a64db9
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-07-06 20:43:07 +0100

    mobile for orders tab in admin dashboard

--- src/app/(pages)/admin/orders/page.tsx
+++ src/app/(pages)/admin/orders/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import useMedia from "@/app/hooks/useMedia";
 import { useEffect, useState } from "react";
 
 interface LineItem {
@@ -25,6 +26,7 @@ interface Order {
 
 export default function Orders() {
 	const [orders, setOrders] = useState<Order[]>([]);
+	const mobile = useMedia("(max-width:767px)");
 
 	useEffect(() => {
 		selectedProduct();
@@ -43,51 +45,55 @@ export default function Orders() {
 	return (
 		<section>
 			<h1 className="h1">Orders</h1>
-			<table className="w-full">
-				<thead>
-					<tr>
-						<td>Data</td>
-						<td>Paid</td>
-						<td>Recipients</td>
-						<td>Products</td>
-					</tr>
-				</thead>
-				<tbody>
-					{orders &&
-						orders.map((order: Order) => (
-							<tr key={order._id}>
-								<td>{order.createdAt}</td>
-								<td
-									className={
-										order.paid
-											? "text-green-600"
-											: "text-red-600"
-									}
-								>
-									{order.paid ? "Yes" : "No"}
-								</td>
+			{!mobile ? (
+				<table className="w-full mx-4">
+					<thead>
+						<tr>
+							<td className="px-2">Data</td>
+							<td className="px-2">Paid</td>
+							<td className="px-2">Recipients</td>
+							<td className="px-2">Products</td>
+						</tr>
+					</thead>
+					<tbody>
+						{orders &&
+							orders.map((order: Order) => (
+								<tr key={order._id}>
+									<td className="px-2">{order.createdAt}</td>
+									<td
+										className={`	${
+											order.paid
+												? "text-green-600"
+												: "text-red-600"
+										}, px-2`}
+									>
+										{order.paid ? "Yes" : "No"}
+									</td>
 
-								<td>
-									{order.name}
-									<br></br>
-									{order.email}
-								</td>
-								<td>
-									{order.line_items.map((line, index) => (
-										<h1 key={index}>
-											{
-												line.price_data?.product_data
-													?.name
-											}{" "}
-											x {line.quantity}
-										</h1>
-									))}
-								</td>
-								{/* <td className="text-center flex gap-2 justify-center mx-2"> */}
-							</tr>
-						))}
-				</tbody>
-			</table>
+									<td className="px-2">
+										{order.name}
+										<br></br>
+										{order.email}
+									</td>
+									<td className="px-2">
+										{order.line_items.map((line, index) => (
+											<h1 key={index}>
+												{
+													line.price_data
+														?.product_data?.name
+												}{" "}
+												x {line.quantity}
+											</h1>
+										))}
+									</td>
+									{/* <td className="text-center flex gap-2 justify-center mx-2"> */}
+								</tr>
+							))}
+					</tbody>
+				</table>
+			) : (
+				<h1 className="m-2"> Please log in a desktop</h1>
+			)}
 		</section>
 	);
 }
